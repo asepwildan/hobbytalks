6ad11cb9ad0fb093bbb80c5ed3c561f8d6afc419
Wahyu Idfa Daru <wahyuidfadaru3@gmail.com>
2021-10-08 22:07:23 +0700

    Login/register new branch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './component/Login/login'
+import BackgroundPage from './component/BackgroundPage/backgroundPage';
+
+function App() {
+  return (
+    <div className="App">
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Register/register.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "./register.module.css"
+import { useState } from "react";
+import google from "../Asset/google.png"
+import facebook from "../Asset/facebook.png"
+
+export default function Register() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handlechange = (e) => {
+    const value = e.target.value;
+    const name = e.target.value;
+    setValues({ ...value, [name]: name });
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    // <div className={styles.mainContainter}>
+    <div className={styles.registerContainer}>
+      <div className={styles.welcomeBoard}>
+        <h3>Join our community!</h3>
+        <p>
+          Already have account?
+          <span style={{ color: "#8AB9D3", cursor: "pointer" }}> Login
+          </span>
+        </p>
+      </div>
+    <div className={styles.formWrapper}>
+      <form className={styles.formRegister} onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input
+          onChange={handlechange}
+          type="name"
+          name="name"
+          value={values.name}
+          required
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          onChange={handlechange}
+          name="email"
+          value={values.email}
+          required
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          onChange={handlechange}
+          name="password"
+          value={values.password}
+          required
+        />
+        <button className={styles.buttonRegister} type="Submit">
+          Sign Up
+        </button>
+      </form>
+      </div>
+      <div className={styles.oAuth}>
+        <button className={styles.authGoogle}><img src={google} alt="google"/> Sign in with Google</button>
+        <button className={styles.authFacebook}><img src={facebook} alt="facebook"/> Login with Facebook</button>
+      </div>
+    </div>
+    // </div>
+  );
+}
