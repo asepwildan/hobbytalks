185c7045d66947936bf9f4b8d03f262e162e6c58
Asep Wildan Nugraha <nugrahaasepwildan@gmail.com>
2021-10-08 21:52:50 +0700

    design footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Hompepage from './pages/homepage/homepage'; 
+import Footer from './component/footer/footer';
 function App() {
   return (
     <div className="App">
         <Hompepage/>  
+        <Footer/>
     </div>
   );
 }
