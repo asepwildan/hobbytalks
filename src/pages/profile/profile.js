64303e19d51cbce35b08d0a0a992bedc6ddd38b4
Asep Wildan Nugraha <nugrahaasepwildan@gmail.com>
2021-10-12 20:44:40 +0700

    add footer temporary

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,22 +1,27 @@
+import React from "react";
 import Thread from "../../component/thread-profile/thread-profile";
 import styles from "./style/profile.module.css";
 import Likerecomendation from "../../component/like-recomendation/like-recomendation";
+import Footer from "../../component/footer/footer";
 
 export default function Profile() {
     return (
-        <div>
-            <h1>Page Profile</h1>
-            <div className={styles.boxContentProfile}>
-                <div className={styles.threadContanerProfile}>
-                    <Thread />
-                </div>
-                <div className={styles.likeRekomContanerProfile}>
-                    <Likerecomendation />
-                    <div className={styles.trendingContanerProfile}>
-                        <h3>trending</h3>
+        <React.Fragment>
+            <div>
+                <h1>Page Profile</h1>
+                <div className={styles.boxContentProfile}>
+                    <div className={styles.threadContanerProfile}>
+                        <Thread />
+                    </div>
+                    <div className={styles.likeRekomContanerProfile}>
+                        <Likerecomendation />
+                        <div className={styles.trendingContanerProfile}>
+                            <h3>trending</h3>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </React.Fragment>
     );
 }
