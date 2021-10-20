0d209b37280fd54d5897840229829de4d5f19eb8
Asep Wildan Nugraha <nugrahaasepwildan@gmail.com>
2021-10-20 22:13:10 +0700

    add account-detail

---
+++ src/pages/account-detail/account-detail.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Thread from "../../component/thread-profile/thread-profile";
+import Likerecomendation from "../../component/like-recomendation/like-recomendation";
+import Footer from "../../component/footer/footer";
+import Navbar from "../../component/Navbar/navbar";
+import Banner from "../../component/banner-profile/Banner";
+import Trending from "../../component/trending/Trending";
+import "./style/Account-detail.scss";
+
+export default function AccountDetail() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <div>
+                <Banner />
+                <div className="boxContentProfile">
+                    <div className="threadContanerProfile">
+                        <Thread />
+                    </div>
+                    <div className="likeRekomContanerProfile">
+                        <Likerecomendation />
+                        <div className="trendingContanerProfile">
+                            <Trending />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </React.Fragment>
+    );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import Profile from "../pages/profile/profile";
 import Category from "../pages/category/Category";
 import LandingGoogle from "../pages/landingGoogle/landingGoogle";
 import ThreadDetail from "../pages/thread-detail/Thread-detail";
+import AccountDetail from "../pages/account-detail/account-detail";
 
 function Routes() {
     return (
@@ -15,6 +16,7 @@ function Routes() {
                     <Route path="/profile" component={Profile} />
                     <Route path="/category" component={Category} />
                     <Route path="/thread-detail" component={ThreadDetail} />
+                    <Route path="/account-detail" component={AccountDetail} />
                     <Route path="/account/:id" component={Backgroundpage} />
                 </Switch>
             </Router>
