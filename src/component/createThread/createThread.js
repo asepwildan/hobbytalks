6fcc16cd77fa3c1e95fb5e7921f59d4c4dab77de
Asep Wildan Nugraha <nugrahaasepwildan@gmail.com>
2021-10-26 16:47:01 +0700

    change position upload img create thread

--- src/component/createThread/createThread.js
+++ src/component/createThread/createThread.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+// import { useEffect, useState, useRef } from "react";
 import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import "./style/createThread.scss";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import CloseIcon from "./close.svg";
-
+// import iconUpload from "../banner-profile/assets/upload.svg";
 const content = {
     entityMap: {},
     blocks: [
@@ -23,10 +24,13 @@ const content = {
 export default class TextEditor extends Component {
     constructor(props) {
         super(props);
+        // this.buttonRef = React.createRef();
         const contentState = convertFromRaw(content);
         this.state = {
             contentState,
+            file: null,
         };
+        this.handleChangeImg = this.handleChangeImg.bind(this);
     }
 
     state = {
@@ -47,12 +51,19 @@ export default class TextEditor extends Component {
     onSubmit = (e) => {
         e.preventDefault();
     };
+    handleChangeImg(img) {
+        this.setState({
+            file: URL.createObjectURL(img.target.files[0]),
+        });
+    }
+
     render() {
         // const { editorState } = this.state;
         const { contentState } = this.state;
         // console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
 
         // console.log(draftToHtml( JSON.stringify(contentState, null, 4));
+
         return (
             <div className="textEditorContainer">
                 <div className="createThread-Title-Container">
@@ -64,12 +75,23 @@ export default class TextEditor extends Component {
                         <label>Title</label>
                         <input type="text" placeholder="Thread title" required />
                     </div>
+                    <label>Image Thread</label>
+                    <div className="createThread-img-container">
+                        <input
+                            type="file"
+                            placeholder="image"
+                            onChange={this.handleChangeImg}
+                            className="custom-file-input"
+                            required
+                        />
+                        <img src={this.state.file} style={{ width: "130px" }} />
+                    </div>
+
                     <label>Story</label>
                     <Editor
-                        // editorStyle={{ visibility: "visible" }}
                         // editorState={editorState}
                         toolbar={{
-                            options: ["inline", "list", "textAlign", "link", "image"],
+                            options: ["inline", "list", "textAlign", "link"],
                             inline: { options: ["bold", "italic", "underline"] },
                             textAlign: { inDropdown: true },
                             link: { inDropdown: true },
@@ -85,21 +107,23 @@ export default class TextEditor extends Component {
                     <div className="label-createThread-category-container">
                         <label>Category</label>
                     </div>
-                    <div className="createThread-option-container">
-                        <select id="category" className="createThread-option" required>
-                            <option value="" selected="selected">
-                                Pilih Category
-                            </option>
-                            <option value="">Travel</option>
-                            <option value="">Game</option>
-                            <option value="">Cooking</option>
-                            <option value="">DIY</option>
-                            <option value="">Sport</option>
-                            <option value="">Music</option>
-                        </select>
-                    </div>
-                    <div className="createThread-PostButton">
-                        <button>Post</button>
+                    <div className="createthrea-optionPost-container">
+                        <div className="createThread-option-container">
+                            <select id="category" className="createThread-option" required>
+                                <option value="" selected="selected">
+                                    Pilih Category
+                                </option>
+                                <option value="">Travel</option>
+                                <option value="">Game</option>
+                                <option value="">Cooking</option>
+                                <option value="">DIY</option>
+                                <option value="">Sport</option>
+                                <option value="">Music</option>
+                            </select>
+                        </div>
+                        <div className="createThread-PostButton">
+                            <button>Post</button>
+                        </div>
                     </div>
                 </form>
 
