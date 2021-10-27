98f562f61b5f564dfcdc8d5da4f4caad6e48d72b
afwan17 <mafwanalfiansyah73@gmail.com>
2021-10-28 01:47:07 +0800

    update condotional thread

--- src/pages/profile/No-thread.js
+++ src/pages/profile/No-thread.js
@@ -1,19 +1,48 @@
-import React from "react";
-import garis from '../../component/assets/logo/garis.svg';
-import nothread from '../../component/assets/images/no.png';
-import './No-thread.scss';
+import React, { useState } from "react";
+import garis from "../../component/assets/logo/garis.svg";
+import nothread from "../../component/assets/images/no.png";
+import TextEditor from "../../component/createThread/createThread";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import "./No-thread.scss";
 
-export default function NoThread () {
-    return(
-        <div className="container">
-            <div className="no-thread">
-                <p className="text">Thread</p>
-                <img className="garis" src={ garis }></img>
-                <img className="notfound" src={ nothread }></img>
-                <p className="text2">You haven't posted any thread yet. </p>
-                <p className="text3">Any interesting story to share? Create a thread now!</p>
-                <button className="tombol">Create a thread</button>
-            </div>
+export default function NoThread() {
+  const [openCt, setOpenCt] = useState();
+  const createThreadFirst = () => {
+    setOpenCt(true);
+  };
+  const closeCreateThread = () => {
+    setOpenCt(false);
+  };
+  return (
+    <div className="container">
+      <div className="no-thread">
+        <p className="text">Thread</p>
+        <div className="garis">
+          <img src={garis}></img>
         </div>
-    )
+        <img className="notfound" src={nothread}></img>
+        <p className="text2">You haven't posted any thread yet. </p>
+        <p className="text3">
+          Any interesting story to share? Create a thread now!
+        </p>
+        <button className="tombol" onClick={createThreadFirst}>
+          Create a thread
+        </button>
+        <Modal
+          keepMounted
+          open={openCt}
+          onClose={closeCreateThread}
+          aria-labelledby="keep-mounted-modal-title"
+          aria-describedby="keep-mounted-modal-description"
+        >
+          <Box>
+            <div className="containerTextEditor">
+              <TextEditor />
+            </div>
+          </Box>
+        </Modal>
+      </div>
+    </div>
+  );
 }
